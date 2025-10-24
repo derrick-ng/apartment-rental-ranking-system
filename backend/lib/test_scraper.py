16824f3bb61eb108e253eef3763ace67baae1dfc
derrick-ng <ng.derrick1@hotmail.com>
2025-10-24 01:58:24 -0700

    Refactor craigslist scrape code to work for multiple pages

--- backend/lib/test_scraper.py
+++ backend/lib/test_scraper.py
@@ -2,43 +2,52 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-url = "https://sfbay.craigslist.org/search/sfc/apa"
+#craigslist page contains only 360 listings (120 rows of 3)
+#function chooses what page to scrape
+def scrape_list_urls(max_pages: int):
+    all_listings = []
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
+    }
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
-}
+    for page in range(max_pages):
+        offset = page * 360
+        url = f"https://sfbay.craigslist.org/search/sfc/apa#search=2~gallery~{offset}"
 
-response = requests.get(url, headers=headers)
-print(f"status code: {response.status_code}")
+        response = requests.get(url, headers=headers)
+        print(f"Status Code: {response.status_code}")
 
-soup = BeautifulSoup(response.content, 'lxml')
-listings = soup.find_all('li', class_='cl-static-search-result')
+        soup = BeautifulSoup(response.content, 'lxml')
+        listings = soup.find_all('li', class_='cl-static-search-result')
 
-print(f"Found {len(listings)} listings\n")
+        for listing in listings:
+            link = listing.find('a')
+            url = link['href'] if link else None
 
-for index, listing in enumerate(listings[:3], 1):
-    link = listing.find('a')
-    url = link['href'] if link else None
+            craigslist_id = url.split('/')[-1].replace('.html', '') if url else None
 
-    craigslist_id = url.split('/')[-1].replace('.html', '') if url else None
+            title_div = listing.find('div', class_='title')
+            title = title_div.text.strip() if title_div else None
 
-    title_div = listing.find('div', class_='title')
-    title = title_div.text.strip() if title_div else None
+            price_div = listing.find('div', class_='price')
+            price_text = price_div.text.strip() if price_div else None
 
-    price_div = listing.find('div', class_='price')
-    price_text = price_div.text.strip() if price_div else None
+            #remove commas and $ in price to cast to int
+            price = None
+            if price_text:
+                price = int(price_text.replace('$', '').replace(',', ''))
 
-    #remove commas in price to cast to int
-    price = None
-    if price_text:
-        price = int(price_text.replace('$', '').replace(',', ''))
+            location_div = listing.find('div', class_='location')
+            location = location_div.text.strip() if location_div else None
 
-    location_div = listing.find('div', class_='location')
-    location = location_div.text.strip() if location_div else None
+            data = {
+                'craigslist_id': craigslist_id,
+                'url': url,
+                'title': title,
+                'price': price,
+                'location': location,
+            }
 
-    print(f"{index} LISTING HTML\n")
-    print(f"ID: {craigslist_id}")
-    print(f"Title: {title}")
-    print(f"Price: ${price}")
-    print(f"Location: {location}")
-    print(f"URL: {url}\n")
+            all_listings.append(data)
+    
+    return all_listings
