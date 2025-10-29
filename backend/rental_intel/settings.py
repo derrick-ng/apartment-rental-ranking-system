b4a7c052da21e211bb01acc89a46ba4e0c2a21b0
derrick-ng <ng.derrick1@hotmail.com>
2025-10-29 02:54:41 -0700

    Fix CORS variable typo

--- backend/rental_intel/settings.py
+++ backend/rental_intel/settings.py
@@ -43,10 +43,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #third party packages/modules
+    'rest_framework',
+    'corsheaders',
+
+    #my apps
     'listings'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +138,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
