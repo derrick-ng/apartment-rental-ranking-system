e5c8677a6c308b3b417565dcb0c8bcbac9c32c60
derrick-ng <ng.derrick1@hotmail.com>
2026-01-22 23:48:48 -0800

    Change api client to point to backend server

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,20 +1,7 @@
 import axios from "axios";
-import { mockListings, mockAnalytics } from "../utils/mockData";
 
-// const api = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
-
-// })
-
-const api = {
-  get: (url) => {
-    if (url.includes("analytics")) {
-      return { data: mockAnalytics };
-    }
-    if (url.includes("listings")) {
-      return { data: mockListings };
-    }
-  },
-};
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
+});
 
 export default api;
